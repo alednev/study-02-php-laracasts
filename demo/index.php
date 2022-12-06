9d9a55fbe2571153625b4d207388d964a03c025a
Alex Lednev <avlednev@vgtrk.com>
2022-12-07 01:37:20 +0300

    template the loop items

--- demo/index.php
+++ demo/index.php
@@ -16,10 +16,9 @@
     ?>
 
     <ul>
-        <?php foreach ($books as $book) {
-          echo "<li>{$book}™</li>";
-        }
-        ?>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
     </ul>
 </body>
 </html>
