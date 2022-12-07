8a8511465230a8f15f7d06dd4c56ea7ebb833c77
Alex Lednev <avlednev@vgtrk.com>
2022-12-07 17:35:01 +0300

    functions and filters homework

--- demo/index.php
+++ demo/index.php
@@ -5,34 +5,55 @@
     <title>Demo</title>
 </head>
 <body>
-<h1>Recommended books</h1>
-
 <?php
 $books = [
     [
         'name'        => 'Do Androids Dream of Electric Sheep',
         'author'      => 'Philip K. Dick',
-        'releaseYear' => '1968',
+        'releaseYear' => 1968,
         'purchaseUrl' => 'http://example.com',
     ],
     [
         'name'        => 'The Langoliers',
         'author'      => 'Stephen King',
-        'releaseYear' => '1990',
+        'releaseYear' => 1990,
         'purchaseUrl' => 'http://example.com',
     ],
     [
         'name'        => 'Hail Mary',
         'author'      => 'Andy Weir',
-        'releaseYear' => '2021',
+        'releaseYear' => 2021,
         'purchaseUrl' => 'http://example.com',
     ],
     [
         'name'        => 'The Martian',
         'author'      => 'Andy Weir',
-        'releaseYear' => '2011',
+        'releaseYear' => 2011,
         'purchaseUrl' => 'http://example.com',
     ]
+];
+
+$movies = [
+    [
+        'title' => 'The Green Mile',
+        'year'  => 1999,
+    ],
+    [
+        'title' => 'The Shawshank Redemption',
+        'year'  => 1994,
+    ],
+    [
+        'title' => 'Forrest Gump',
+        'year'  => 1994,
+    ],
+    [
+        'title' => 'The Lord of the Rings: The Return of the King',
+        'year'  => 2003,
+    ],
+    [
+        'title' => 'Interstellar',
+        'year'  => 2014,
+    ],
 ];
 
 function filterByAuthor($books, $author) {
@@ -46,7 +67,21 @@ function filterByAuthor($books, $author) {
 
     return $filteredBooks;
 }
+
+function filterMoviesByYear($movies, $year) {
+    $filteredMovies = [];
+
+    foreach ($movies as $movie) {
+        if ($movie['year'] >= $year) {
+            $filteredMovies[] = $movie;
+        }
+    }
+
+    return $filteredMovies;
+}
 ?>
+
+<h1>Recommended books</h1>
 
 <ul>
     <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
@@ -54,6 +89,16 @@ function filterByAuthor($books, $author) {
             <a href="<?= $book['purchaseUrl'] ?>">
                 <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
             </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+<h1>Recommended movies</h1>
+
+<ul>
+    <?php foreach (filterMoviesByYear($movies, 2000) as $movie) : ?>
+        <li>
+            <?= $movie['title'] ?> (<?= $movie['year'] ?>)
         </li>
     <?php endforeach; ?>
 </ul>
