b48aae20e67ef4f5e1e2db820a639112cde73ee2
Alex Lednev <avlednev@vgtrk.com>
2022-12-08 18:50:46 +0300

    extract all other staff in theirs files

--- demo 2/views/about.view.php
+++ demo 2/views/about.view.php
@@ -1,27 +1,13 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <title>Section 2</title>
-    <script src="https://cdn.tailwindcss.com/"></script>
-</head>
-<body class="h-full">
+<?php require('partials/head.php') ?>
 
-<div class="min-h-full">
+<?php require('partials/nav.php') ?>
 
-    <?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
 
-    <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>Now you are on About page.</p>
-        </div>
-    </main>
-</div>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Now you are on About page.</p>
+    </div>
+</main>
 
-</body>
-</html>
+<?php require('partials/foot.php') ?>
--- demo 2/views/mission.view.php
+++ demo 2/views/mission.view.php
@@ -1,27 +1,11 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <title>Section 2</title>
-    <script src="https://cdn.tailwindcss.com/"></script>
-</head>
-<body class="h-full">
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
 
-<div class="min-h-full">
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>Now you are on Mission page.</p>
+    </div>
+</main>
 
-    <?php require('partials/nav.php') ?>
-
-    <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Our Mission</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>Our mission is in...</p>
-        </div>
-    </main>
-</div>
-
-</body>
-</html>
+<?php require('partials/foot.php') ?>
