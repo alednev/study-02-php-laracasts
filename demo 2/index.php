c44688b27fafa7517b181c39e0d75cc326f806a5
Alex Lednev <avlednev@vgtrk.com>
2022-12-08 23:13:20 +0300

    db query homework

--- demo 2/index.php
+++ demo 2/index.php
@@ -8,11 +8,13 @@ require "functions.php";
 $dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
 $pdo = new PDO($dsn);
 
-$statement = $pdo->prepare('select * from posts');
+$statement = $pdo->prepare('select * from posts where id = 1');
 $statement->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $statement->fetch(PDO::FETCH_ASSOC);
 
-foreach ($posts as $index => $post) {
-    echo '<li>' . $post['title'] . '</li>';
-}
+dd($posts);
+
+//foreach ($posts as $index => $post) {
+//    echo '<li>' . $post['title'] . '</li>';
+//}
