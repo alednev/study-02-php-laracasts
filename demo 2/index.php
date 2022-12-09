f0cc779c64196c7a24c348ba1283d713c8b116d8
Alex Lednev <avlednev@vgtrk.com>
2022-12-09 18:04:54 +0300

    extract db class

--- demo 2/index.php
+++ demo 2/index.php
@@ -2,19 +2,11 @@
 
 require "functions.php";
 //require "router.php";
+require "Database.php";
 
-// connecting to a DataBase
+$db = new Database();
+$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
 
-$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare('select * from posts where id = 1');
-$statement->execute();
-
-$posts = $statement->fetch(PDO::FETCH_ASSOC);
-
-dd($posts);
-
-//foreach ($posts as $index => $post) {
-//    echo '<li>' . $post['title'] . '</li>';
-//}
+foreach ($posts as $index => $post) {
+    echo '<li>' . $post['title'] . '</li>';
+}
